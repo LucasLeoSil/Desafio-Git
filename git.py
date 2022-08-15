031ac1505ddc5324fae9618dbfb63065bee21837
Lucas <lucascomercialestudantil@gmail.com>
2022-08-15 17:37:38 -0300

    Consertado

---
+++ git.py
@@ -0,0 +1,23 @@
+def search(arr, n, x):
+	final = False
+	for i in range(0, n):
+		if arr[i]==x:
+			final = i
+
+	if final!=False:
+		return final
+	else:
+		return -1
+
+
+
+arr = [2, 3, 4, 10, 40]
+x = 10
+n = len(arr)
+
+# Function call
+result = search(arr, n, x)
+if(result == -1):
+	print("Element is not present in array")
+else:
+	print("Element is present at index", result)
